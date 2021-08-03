8ae0890d43a4c82fbb04b53435078120674cb548
Tom Meadon <tom@meadon.me>
2021-08-03 16:30:03 +0100

    fix script

--- k8s/scripts/install_dapr.sh
+++ k8s/scripts/install_dapr.sh
@@ -2,8 +2,8 @@
 
 helm repo add dapr https://dapr.github.io/helm-charts/
 helm repo update
-helm upgrade --install dapr dapr/dapr `
---version=1.3 `
---namespace dapr-system `
---create-namespace `
+helm upgrade --install dapr dapr/dapr \
+--version=1.3 \
+--namespace dapr-system \
+--create-namespace \
 --wait
