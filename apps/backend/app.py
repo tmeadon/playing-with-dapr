a2780812eba3f73c83551f36079d90f4234efb25
Tom Meadon <tom@meadon.me>
2021-08-04 11:02:10 +0100

    add pubsub receive endpoint

--- apps/backend/app.py
+++ apps/backend/app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 
 @app.route('/httpReceive', methods = ['POST'])
 def httpReceive():
+    print("received via http")
     return request.get_json()
+
+@app.route('/pubsubReceive', methods = ['POST'])
+def pubsubReceive():
+    print("received via pubsub: {}".format(request.get_json()))
 
 app.run()
