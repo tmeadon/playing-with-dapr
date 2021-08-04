ed2452c360ffdf12e76cc60d4aa089f4db601c9c
Tom Meadon <tom@meadon.me>
2021-08-04 11:12:18 +0100

    logging

--- apps/backend/app.py
+++ apps/backend/app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 @app.route('/httpReceive', methods = ['POST'])
 def httpReceive():
-    print("received via http")
+    print("received via http", flush=True)
     return request.get_json()
 
 @app.route('/pubsubReceive', methods = ['POST'])
 def pubsubReceive():
-    print("received via pubsub: {}".format(request.get_json()))
+    print("received via pubsub: {}".format(request.get_json()), flush=True)
     return Response({'success':True}, HTTPStatus.OK, {'ContentType':'application/json'})
 
 app.run()
