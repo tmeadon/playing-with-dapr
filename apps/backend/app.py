7576139b0e48a255619b324e253fa762beaf3820
Tom Meadon <tom@meadon.me>
2021-08-04 11:19:59 +0100

    write data to cosmos

--- apps/backend/app.py
+++ apps/backend/app.py
@@ -1,3 +1,4 @@
+from dapr.clients import DaprClient
 from http import HTTPStatus
 from flask import Flask, request
 from dapr.clients import DaprClient
@@ -7,12 +8,20 @@ app = Flask(__name__)
 
 @app.route('/httpReceive', methods = ['POST'])
 def httpReceive():
-    print("received via http", flush=True)
+    body = request.json
+    print("received via http {0}".format(body), flush=True)
+    with DaprClient() as d:
+        d.save_state(store_name="backend0", key=body["id"], value=body)
+        print("saved state to backend0", flush=True)
     return request.get_json()
 
 @app.route('/pubsubReceive', methods = ['POST'])
 def pubsubReceive():
-    print("received via pubsub: {}".format(request.get_json()), flush=True)
+    body = request.json
+    print("received via pubsub: {}".format(body), flush=True)
+    with DaprClient() as d:
+        d.save_state(store_name="backend1", key=body["id"], value=body)
+        print("saved state to backend1", flush=True)
     return Response({'success':True}, HTTPStatus.OK, {'ContentType':'application/json'})
 
 app.run()
