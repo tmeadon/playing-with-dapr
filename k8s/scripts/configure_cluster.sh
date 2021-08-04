8dd60e43de99399e874a536b2b6b03f05882ac6c
Tom Meadon <tom@meadon.me>
2021-08-04 09:13:06 +0100

    add debug to acr attach step

--- k8s/scripts/configure_cluster.sh
+++ k8s/scripts/configure_cluster.sh
@@ -2,7 +2,7 @@
 
 az extension add --name aks-preview
 az extension update --name aks-preview
-az aks update --resource-group $RSG_NAME --name $AKS_CLUSTER_NAME --attach-acr $ACR_NAME
+az aks update --resource-group $RSG_NAME --name $AKS_CLUSTER_NAME --attach-acr $ACR_NAME --debug
 az aks update --resource-group $RSG_NAME --name $AKS_CLUSTER_NAME --enable-pod-identity
 IDENTITY_RESOURCE_ID="$(az identity show -g $RSG_NAME -n $MSI_NAME --query id -otsv)"
 az aks pod-identity add --resource-group $RSG_NAME --cluster-name $AKS_CLUSTER_NAME --namespace 'default' --name msi --identity-resource-id $IDENTITY_RESOURCE_ID
